7f074dd6fee7af947c29428df9fc7381a8f9078b
David McDougal <davidmcdougal@Davids-MacBook-Pro-Site-Source.local>
2023-08-29 00:39:31 -0600

    pushing up changes for the night 1:17:35

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1743e8543bf577f0"; }
+/******/ 		__webpack_require__.h = function() { return "da713840c4a83217"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
