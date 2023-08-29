f755dc46a91cbdd158a1468cd395abc2dc7b90bf
David McDougal <davidmcdougal@Davids-MacBook-Pro-Site-Source.local>
2023-08-28 22:41:12 -0600

    finished the first part of the nav bar 48:44

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "78f29008787bd25e"; }
+/******/ 		__webpack_require__.h = function() { return "8284eae91c670ca6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
